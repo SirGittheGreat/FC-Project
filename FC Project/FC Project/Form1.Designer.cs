bc2a319451be6f3b97711555132be2e7de7fd92c
SirGittheGreat <jarrad.simpson.js@gmail.com>
2016-06-22 09:41:53 +1000

    Open Coms Button

---
+++ FC Project/FC Project/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace FC_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenCOMs = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OpenCOMs
+            // 
+            this.OpenCOMs.Location = new System.Drawing.Point(195, 29);
+            this.OpenCOMs.Name = "OpenCOMs";
+            this.OpenCOMs.Size = new System.Drawing.Size(112, 23);
+            this.OpenCOMs.TabIndex = 0;
+            this.OpenCOMs.Text = "Open COM Port";
+            this.OpenCOMs.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 375);
+            this.Controls.Add(this.OpenCOMs);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenCOMs;
+    }
+}
+
